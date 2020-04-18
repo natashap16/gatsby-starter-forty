505412e3ab43f2244111a523b7156b8f10da9410
natashap16 <ncprats@gmail.com>
2020-04-18 18:38:00 -0400

    work on resume page

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const BannerLanding = (props) => (
-    <section id="banner" className="style2">
+    <section id="banner" className="style1">
         <div className="inner">
             <header className="major">
                 <h1>{props.projectTitle}</h1>
