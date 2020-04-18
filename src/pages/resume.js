6424098fc5131f5593b5f372c79c2006613fceae
natashap16 <ncprats@gmail.com>
2020-04-18 19:25:23 -0400

    reverting resume page back....this must work right?

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 
-import PMrole from '../assets/images/pm_image.jpg'
+import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
@@ -15,7 +15,7 @@ const Landing = (props) => (
             <meta name="description" content="Landing Page" />
         </Helmet>
 
-        <BannerLanding projectTitle="Resume" projectTopDesc="BS, MA from Stanford. 10 years in tech, with a varierty of experience across PM, UX, Ops, and Advertising"/>
+        <BannerLanding projectTitle="Resume" projectTopDesc="Work in progress - stay tuned"/>
 
         <div id="main">
             <section id="one">
@@ -28,31 +28,17 @@ const Landing = (props) => (
             </section>
             <section id="two" className="spotlights">
                 <section>
-                        <Link to="/generic" className="image">
-                            <img src={PMrole} alt="" />
-                        </Link>
+                    <Link to="/generic" className="image">
+                        <img src={pic08} alt="" />
+                    </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3><a href="https://www.pegs.com/">Pegasus</a> / Travel Tripper</h3>
+                                <h3>Orci maecenas</h3>
                             </header>
-                            <h4>Director of Product: 2019 - current</h4>
-                            <ul>
-                                <li>Won approval for, and now leading, next-generation hotel tool redesign, based on close inter-departmental collaboration and research</li>
-                                <li>Managing a team of PMs and UX Designers</li>
-                            </ul>
-                            <h4>Associate Director of Product: 2018 - 2019</h4>
-                            <ul>
-                                <li>Travel Tripper merges with Pegasus. I join the transition team to lead the merging of our products.</li>
-                            </ul>
-                            <h4>Product Manager: 2015 - 2018</h4>
-                            <header className="major">
-                                <h3><a href="https://intent.com/">Intent Media</a></h3>
-                            </header>
-                            <h4>Product Manager: 2015</h4>
-                            <ul>
-                                <li>Evaluated multivariate testing program via user and market research</li>
-                                <li>Led design, development, and launch of improved multivariate testing program</li>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
                             </ul>
                         </div>
                     </div>
@@ -64,15 +50,12 @@ const Landing = (props) => (
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Design Consultant</h3>
+                                <h3>Rhoncus magna</h3>
                             </header>
-                            <h4>AltSchool: 2014</h4>
-                            <p>UX research. Usability testing & teacher time lapse study.</p>
-                            <h4><a href="https://www.slicedbreaddesign.com/">Sliced Bread Design</a>: 2014</h4>
-                            <p>Focus on file storage & sharing software. Research -> Design -> Prototype via HTML, CSS, JS -> Test</p>
-                            <h4><a href="https://www.fluentcity.com/">Fluent City</a>: 2014</h4>
-                            <p>Survey & followup in-person generative research sessions, to define 4 core personas for language learning company.</p>
-                            <h4>& more</h4>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                            </ul>
                         </div>
                     </div>
                 </section>
