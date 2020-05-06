2ea34b0c9303f398ed8bd23e872986e55e3568d5
natashap16 <ncprats@gmail.com>
2020-05-06 14:55:16 -0400

    some work on ux research page

--- src/pages/UXResearch.js
+++ src/pages/UXResearch.js
@@ -4,8 +4,8 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 
-import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
+import googleUX from '../assets/images/ux_research/usability-testing-google.jpg'
+import altschoolUX from '../assets/images/ux_research/time-lapse-altschool.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
 const Landing = (props) => (
@@ -21,41 +21,41 @@ const Landing = (props) => (
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h2>Sed amet aliquam</h2>
+                        <h2>Overview</h2>
                     </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                    <p>I've always loved psychology, and user experience research is a great extension of that. From brainstorming how to design a study that minimizes bias, to those wonderful observations and surprises you encounter, I'm well-versed in both generative and evaluate user research.</p>
+                    <p>Sidenote: Have you ever read I Never Promised You a Rose Garden? It's great, and was my first glimpse into mental illness. Fascinating!</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
                 <section>
                     <Link to="/generic" className="image">
-                        <img src={pic08} alt="" />
+                        <img src={googleUX} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Orci maecenas</h3>
+                                <h3>Testing at Google</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                            <p>During my 3+ years on Google Operations, I did a few 20% projects in UX research. I designed, recruited for, and evaluated usability studies for:</p>
+                            <ul>
+                                <li>socialization within Google Shopping</li>
+                                <li>consumer setup experience for Google Fiber</li>
+                                <li>merchant data input within Google Merchant Center</li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <section>
                     <Link to="/generic" className="image">
-                        <img src={pic09} alt="" />
+                        <img src={altschoolUX} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Rhoncus magna</h3>
+                                <h3>AltSchool (now Altitude Learning)</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
+                            <p>During grad school, I interned at AltSchool during its early days, where I did my first time lapse study. I observed individual and group classroom activities to explain to broader team what goes on in classroom.</p>
                         </div>
                     </div>
                 </section>
