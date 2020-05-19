72442e9a2bb50a8b70aa0c9237b74b9d61921c7a
natashap16 <ncprats@gmail.com>
2020-05-19 13:47:10 -0400

    updated socialize page & new summary on resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -20,10 +20,7 @@ const Landing = (props) => (
         <div id="main">
             <section id="one">
                 <div className="inner">
-                    <header className="major">
-                        <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                    <p>I've been working in tech for over a decade. Below you can review my experience by functional area, but the common theme through my career is a focus on the user.</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
--- src/pages/socialize.js
+++ src/pages/socialize.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
 import pic11 from '../assets/images/pic11.jpg'
+import flow from '../assets/images/PD-stanford/socialize-flow.png'
 
 const Generic = (props) => (
     <Layout>
@@ -11,13 +12,15 @@ const Generic = (props) => (
             <meta name="description" content="Socialize mobile app project" />
         </Helmet>
 
-        <div id="main" className="alt">
+        <div id="main2" className="alt">
             <section id="one">
                 <div className="inner">
-                    <header className="major">
-                        <h1>Socialize</h1>
+                    <header className="projectTitle">
+                        <h1>Socialize: Broadcast & find hyperlocal events with friends</h1>
                     </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
+                    <span className="image main">
+                        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTh0ApKWo8TeF-Yl9JBFPLanTfjqKBR7WUrTH0KBz_eMSyovlo7dGGWAgRUSU2ap5kCDR2d5-BAcHHe/embed?start=false&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    </span>
                     <h2>Summary</h2>
                     <div className="grid-wrapper">
                         <div className="col-6">
@@ -30,6 +33,28 @@ const Generic = (props) => (
                             <p><span class="boldText">When:</span> Winter 2014 (Stanford CS147 class)</p>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section id="two" class="altSection">
+                <div className="inner">
+                    <h2>Design Process</h2>
+                    <ol>
+                        <li><span class="boldText">INITIAL IDEA:</span> We started with an idea of extremely simplified event creation with a very visual interface to show when events were happening.</li>
+                        <li><span class="boldText">USER TESTING - PROTOTYPES:</span> From initial user testing with paper prototypes and early functional prototypes, two big themes emerged:
+                            <ul>
+                                <li>Users wanted the flexibility to add more details if necessary --> We added optional fields for location address or venue name, duration, and more.</li>
+                                <li>Users wanted to see who was going. --> We made it easier to see who else had joined an event., by showing names and pictures on the event details page and an attendee count from the event feed.</li>
+                            </ul>
+                        </li>
+                        <li><span class="boldText">A/B TESTING:</span> Since this app was about socialization, we conducted A/B testing to determine which of two menu interfaces led to an increased number of event joins.
+                            <ul>
+                                <li><span class="boldText">Who:</span> A dorm of ~40 people at Stanford.</li>
+                                <li><span class="boldText">How:</span> Users were asked to randomly choose link 1 or 2 from an email to determine which version they saw. (Ended up with 20-30 visitors for each version.)</li>
+                                <li><span class="boldText">Results:</span> Ultimately, we didn't see a significant difference in bounce rate, page views, event joins, or other markers, so we proceeded based on qualitative feedback that said users preferred the visual styling of the menu implemented above.</li>
+                                <li><span class="boldText">Impact:</span>Because this round of user testing was conducted with dorm members actually using the app, this was a good proof-of-concept of the initial need. We received a lot of positive feedback.</li>
+                            </ul>
+                        </li>
+                    </ol>
                 </div>
             </section>
         </div>
